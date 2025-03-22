dce32b7954620ddb9c8faa7a8691d64e40d77a8b
Matthew Wen <mattwen2018@gmail.com>
2025-03-22 12:03:11 -0700

    only allow cloudflare connections for backend

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,20 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request, HTTPException
+from fastapi.responses import JSONResponse
+import os
 
 app = FastAPI()
+
+ALLOWED_IP = "10.214.209.4"
+@app.middleware("http")
+async def check_ip_address(request: Request, call_next):
+    client_ip = request.client.host
+    is_production  = os.getenv("PRODUCTION", "false") == "true"
+
+    if is_production and client_ip != ALLOWED_IP:
+        raise HTTPException(status_code=403, detail="Forbidden: Access denied")
+
+    response = await call_next(request)
+    return response
 
 @app.get("/")
 def read_root():
